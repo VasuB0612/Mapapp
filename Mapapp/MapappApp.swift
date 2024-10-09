5ee0e4922d47949e78707124fb85432a5cc62e9b
Token <vasu.bhatnagar@tuni.fi>
2024-10-09 15:33:29 +0300

    Getting on with the map app

---
+++ Mapapp/MapappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapappApp.swift
+//  Mapapp
+//
+//  Created by Vasu Bhatnagar on 9.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MapappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapappTests/MapappTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapappTests.swift
+//  MapappTests
+//
+//  Created by Vasu Bhatnagar on 9.10.2024.
+//
+
+import Testing
+@testable import Mapapp
+
+struct MapappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
